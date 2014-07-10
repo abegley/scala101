2d4010460834b4d0d9939510aa75ffabddfd81aa
Alan Begley <begleyalan@gmail.com>
2014-07-10 23:30:17 +0100

    5.7 The primary constructor

--- src/main/scala/Constructors.scala
+++ src/main/scala/Constructors.scala
@@ -3,7 +3,7 @@ package main.scala
 /**
  In summary,
  -  Scala class can have many contructors
-  - Primary constructor
+  - Primary constructor - not defined with 'this'.  Instead, it is interwoven with the class definition.
   - AuxiliaryConstructors - called 'this' - must start with a call to a previously defined auxiliary constructor
     or the primary constructor.
  */
@@ -30,6 +30,19 @@ object AuxiliaryConstructorsApp{
     val auxConstructor2 = new AuxiliaryConstructors("Stephanie")
 
     val primaryConstructor = new AuxiliaryConstructors()
+  }
+}
 
+//The parameters of the primary constructor are placed immediately after the class name
+//Notes:  Parameters of the primary constructor turn into fields that are initialized with the construction parameters.
+//The parameters 'name' and 'age' become fields of the PrimaryConstructor class
+class PrimaryConstructor(val name: String, val age : Int){
+  println("Just called primary constructor")
+  def description = name + " is " + age + " years old"
+}
+
+object PrimaryConstructorApp{
+  def main(args: Array[String]){
+    println(new PrimaryConstructor("Alan", 30).description)
   }
 }
