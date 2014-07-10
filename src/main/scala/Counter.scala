b941e6a689c59c1933a254027ff94031c7b08b4f
Alan Begley <begleyalan@gmail.com>
2014-07-10 22:32:33 +0100

    5.4 object-private fields and 5.5 Bean Properties

--- src/main/scala/Counter.scala
+++ src/main/scala/Counter.scala
@@ -2,8 +2,12 @@ package main.scala
 
 class Counter {
   private var value=0  //must initialize the field
+  private[this] var restrictedValue = 0; //Accessing someObject.value is not allowed
   def increment(){   //methods are public by default
     value+=15
   }
   def current()=value
+
+  def isLess(other: Counter) = value < other.value //Can access private field of other object
+  //Note, the method above cannot access restrictedValue, see object-private
 }
--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,17 +1,28 @@
 package main.scala
 
+import scala.beans.BeanProperty
+
 class Person {
   private var privateAge = 0
+
+  @BeanProperty var name: String = _
+  //The above field generates four methods
+  //1. name: String
+  //2. name_=(newValue: String): Unit
+  //3. getName(): String
+  //4. setName(newValue: String): Unit
 
   def age = privateAge
   def age_=(newValue: Int){
     if(newValue > privateAge) privateAge = newValue; //Can't get younger
   }
+}
 
+object PersonApp {
   def main(args: Array[String]){
     val fred = new Person
     fred.age = 30
     fred.age = 21
-    println(fred.age)//30
+    println(fred.age)  //30
   }
 }
