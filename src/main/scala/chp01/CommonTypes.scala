3e68ba2c2d5a46823c2f50a4f35ae5437e90ef31
Alan Begley <begleyalan@gmail.com>
2014-07-26 10:23:23 +0100

    amend

--- src/main/scala/chp01/CommonTypes.scala
+++ src/main/scala/chp01/CommonTypes.scala
@@ -8,6 +8,7 @@ NOTES:
     - Seven numeric types, all classes, Byte, Char, Short, Int, Long, Float and Double
     - no need for wrapper types, scala compiler converts between primitive types and wrappers
     - For example, if you make an array of Int, you get an int[] array in the virtual machine
+    - use methods, not casts, to convert between numeric types
  */
 object CommonTypes {
   def main(args:  Array[String]){
@@ -26,5 +27,6 @@ object CommonTypes {
     println(99.4567.toInt)
     println(99.64321.toInt)
     println(90.toChar)
+    println("99.22".toDouble * 2)
   }
 }
