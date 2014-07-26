7f47c6533216c6ca1c2b19176b68786153c9c2c2
Alan Begley <begleyalan@gmail.com>
2014-07-26 11:41:53 +0100

    Chp02 2.4 Input and Output

---
+++ src/main/scala/chp02/InputAndOutput.scala
@@ -0,0 +1,17 @@
+package main.scala.chp02
+
+import scala.io.ReadStdin._
+
+/*
+NOTES:
+
+ */
+object InputAndOutput {
+  def main(args: Array[String]){}
+    printf("Hello, %s! You are %d years old.\n", "Fred", 42)
+
+    val name = readLine("Your Name: ")
+    print("Your age: ")
+    val age = readInt()
+    printf("Hello, %s! Next year, you will be %d.\n", name, age +1)
+}
