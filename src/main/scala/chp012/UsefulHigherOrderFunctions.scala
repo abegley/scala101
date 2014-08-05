6081675fcace39a58395b0e835f5c63b77e6ff64
begleyalan <begleyalan@gmail.com>
2014-08-05 14:13:54 +0100

    12.5 Useful Higher Order functions - filter example

--- src/main/scala/chp012/UsefulHigherOrderFunctions.scala
+++ src/main/scala/chp012/UsefulHigherOrderFunctions.scala
@@ -6,6 +6,12 @@ package chp012
 object UsefulHigherOrderFunctions {
   def main(args: Array[String]){
     (1 to 20).map("*" * _).foreach(println _)
+    (1 to 20).map("*" * _).foreach(println _)
+
+
+    //The filter method yields all elements that match a particular condition.  For
+    //example, here's how to get only even numbers
+    (1 to 9).filter(_ % 2 == 0).foreach(println _)
   }
 
 }
