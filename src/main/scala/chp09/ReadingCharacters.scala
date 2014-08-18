be63b6a4965071986f5412cb0290576a3d4add13
Alan <begleyalan@gmail.com>
2014-08-18 21:09:54 +0100

    Chp 9 Reading Characters

--- src/main/scala/chp09/ReadingCharacters.scala
+++ src/main/scala/chp09/ReadingCharacters.scala
@@ -1,8 +1,23 @@
 package chp09
 
+import scala.io.Source
+
 /**
  * Created by Alan on 18/08/2014.
  */
-class ReadingCharacters {
+object ReadingCharacters extends App {
+
+  val source = Source.fromFile("src/main/resources/nasdaq100list.csv", "UTF-8")
+  val iter = source.buffered
+  /*while(iter.hasNext){
+    if(iter.head)
+      iter.next()
+    else
+      println(iter.length)
+  }*/
+
+  val contents = source.mkString
+  source.close()
+  println(contents)
 
 }
