1acc201bccc93be3a57ef96f920248e085106e62
begleyalan <begleyalan@gmail.com>
2014-08-19 15:12:54 +0100

    Recipe 9.2 Added Discussion section

--- src/main/scala/scratch/FunctionsAsVariables.scala
+++ src/main/scala/scratch/FunctionsAsVariables.scala
@@ -31,6 +31,50 @@ object FunctionsAsVariables extends App {
   println("elements in list doubled")
   list.map(double).foreach(println(_))
 
+  //Discussion
+  //You can declare a function literal in at least two different ways.
+  //I generally prefer the following approach, which implicitly infers that the following function’s return type is Boolean:
+  val f = (i:Int) => {i % 2 == 0 }
+
+  //In this case, the Scala compiler is smart enough to look at the body of the function and determine that it returns a Boolean value.
+  //As a human, it’s also easy to look at the code on the right side of the expression and see that it returns a Boolean,
+  //so I usually leave the explicit Boolean return type off the function declaration. However, if you prefer to
+  //explicitly declare the return type of a function literal, or want to do so because your function is more complex,
+  //the following examples show different forms you can use to explicitly declare that your function returns a Boolean:
+
+  val f1: (Int) => Boolean = i => { i % 2 == 0}
+  val f2: Int => Boolean = i => { i % 2 == 0}
+  val f3: Int => Boolean = i => i % 2 == 0
+  val f4: Int => Boolean = _ % 2 == 0
+
+  //A second example helps demonstrate the difference of these approaches. These functions all take two Int parameters
+  //and return a single Int value, which is the sum of the two input values:
+
+  //implicit approach
+  val add1 = (x: Int, y:Int) => {x + y}
+  val add2 = (x:Int, y:Int) => x + y
+
+  //explicit approach
+  val add3: (Int,Int) => Int = (x,y) => {x +y}
+  val add4: (Int, Int) => Int = (x,y) => x+y
+
+  //As shown, the curly braces around the body of the function in these simple examples are optional, but they are required
+  //when the function body grows to more than one expression:
+  val addThenDouble: (Int, Int) => Int = (x,y) => {
+    val a = x + y
+    2 * a
+  }
+
+  //Scala is very flexible, and just like you can define an anonymous function and assign it to a variable,
+  //you can also define a method and then pass it around like an instance variable. Again using a modulus example, you
+  //can define a method in any of these ways:
+  def modMethod1(i:Int) = i % 2 == 0
+  def modMethod2(i:Int) = { i % 2 == 0}
+  def modMethod3(i:Int): Boolean = i % 2 == 0
+  def modMethod4(i:Int): Boolean = { i % 2 == 0}
+
+  val list1 = List.range(1,10)
+  list1.filter(modMethod1).foreach(println(_))
 
 
 
